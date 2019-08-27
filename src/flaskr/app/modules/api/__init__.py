e97cc84d76499ac2195edb3b2b7cad908679efa0
Ricardo Figueroa <rfigueroa@intersysconsulting.com>
2019-08-27 16:32:48 -0500

    Fixed Aliasing Problems

--- src/flaskr/app/modules/api/__init__.py
+++ src/flaskr/app/modules/api/__init__.py
@@ -1,6 +1,8 @@
 from flask_restplus import Api, Namespace, Resource
 # When you finish your class, add it under this one.
-import .product as product
+from .product import any_ns   as product_any_ns
+from .product import admin_ns as product_admin_ns
+
 
 # Keep this as is for now
 api = Api(
@@ -10,7 +12,7 @@ api = Api(
 )
 
 # After you import your namespaces, import them into the API
-api.add_namespace(product.any_ns)
-api.add_namespace(product.admin_ns)
+api.add_namespace(product_any_ns)
+api.add_namespace(product_admin_ns)
 
 # We will probably add in Blueprinting for versioning of API, but Blueprinting for now should be unnecessary
