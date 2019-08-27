9d2b54dc9a2ce2e9a8c975b31d7f6a645b5269fd
Pablo Rivera <privera@intersysconsulting.com>
2019-08-27 11:16:07 -0500

    made orders file

--- src/flaskr/app/modules/api/__init__.py
+++ src/flaskr/app/modules/api/__init__.py
@@ -1,6 +1,7 @@
 from flask_restplus import Api, Namespace, Resource
 # When you finish your class, add it under this one.
 from .product import any_ns, admin_ns 
+from .order import any_ns as order, admin_ns as admin_order
 
 # Keep this as is for now
 api = Api(
@@ -12,5 +13,7 @@ api = Api(
 # After you import your namespaces, import them into the API
 api.add_namespace(any_ns)
 api.add_namespace(admin_ns)
+api.add_namespace(order)
+api.add_namespace(admin_order)
 
 # We will probably add in Blueprinting for versioning of API, but Blueprinting for now should be unnecessary
