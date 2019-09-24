19b44f37e284f3f3c7011db43266e334e11e7b64
Ricardo Figueroa <rfigueroa@intersysconsulting.com>
2019-09-24 14:39:20 -0500

    Fixed database connection

--- src/flaskr/app/modules/db/mongo.py
+++ src/flaskr/app/modules/db/mongo.py
@@ -3,7 +3,6 @@ from pymongo import MongoClient, ASCENDING, DESCENDING
 from urllib.parse import quote_plus
 
 
-
 class Mongo:
     def __init__(self):
         protocol = 'mongodb+srv://'
@@ -12,34 +11,45 @@ class Mongo:
         options = 'test?retryWrites=true&w=majority'
         database = 'ecommerce'
 
-        mongo_url = "{0}{1}:{2}@{3}/{4}".format(protocol, user, password, server, options)
+        mongo_url = "{0}{1}:{2}@{3}/{4}".format(protocol, user, password,
+                                                server, options)
         # protocol = 'mongodb://'
         # user = 'intersys'
         # password = 's3cureP@ssw0rd'
         # server = '127.0.0.1'
-        # port = '27001'        
+        # port = '27001'
         # database = webproject
         # mongo_url = protocol + user + ':' + quote_plus(password) + '@' + server + ':' + port
 
         self.db = MongoClient(mongo_url)[database]
 
-    def find_all(self, table, selector, sort, _ascending, _next_page, _page_size):  
-        if(sort!=None):
-            # If it requires sorting
-            sortType = ASCENDING if _ascending else DESCENDING            
-            return self.db[table].find(selector).sort(sort, sortType).skip(_next_page*_page_size).limit(_page_size)
-        # If it is unsorted             
-        return self.db[table].find(selector).skip(_next_page*_page_size).limit(_page_size)
+    def find_all(self, collection, selector, sort, _ascending, _next_page,
+                 _page_size):
+        print("Paremeters. _ascending {}, _next_page {}, _page_size {}".format(
+            _ascending, _next_page, _page_size))
+        allDocuments = []
+        for document in self.db[collection].find(selector).skip(
+                _next_page * _page_size).limit(_page_size):
+            allDocuments.append(document)
 
-    def find(self, table, selector):        
-        return self.db[table].find_one(selector)
+        return allDocuments
+        # if (sort != None):
+        #     # If it requires sorting
+        #     sortType = ASCENDING if _ascending else DESCENDING
+        #     return self.db[collection].find(selector).sort(
+        #         sort, sortType).skip(_next_page * _page_size).limit(_page_size)
+        # # If it is unsorted
+        # return self.db[collection].find(selector).skip(
+        #     _next_page * _page_size).limit(_page_size)
 
-    def create(self, table, user):
-        return self.db[table].insert_one(user)
-        
-    def update(self, table, selector, element):
-        # return self.db[table].replace_one(selector, user).modified_count
-        return self.db[table].update_one(selector, element).modified_count
+    def find(self, collection, selector):
+        return self.db[collection].find_one(selector)
 
-    def delete(self, table, selector):
-            return self.db[table].delete_one(selector).deleted_count
+    def create(self, collection, user):
+        return self.db[collection].insert_one(user)
+
+    def update(self, collection, selector, user):
+        return self.db[collection].replace_one(selector, user).modified_count
+
+    def delete(self, collection, selector):
+        return self.db[collection].delete_one(selector).deleted_count
--- src/flaskr/app/modules/resources/product/__init__.py
+++ src/flaskr/app/modules/resources/product/__init__.py
@@ -3,36 +3,41 @@ from flask_restplus import Resource
 from ...db import Database
 
 # In this file we implement methods that serve as a middle point between the exposed endpoint and the
-# actual middleware. 
-# Validate operation logic here, not permission to access it.  
+# actual middleware.
+# Validate operation logic here, not permission to access it.
 
-class UserProduct(): 
-    
-    def __init__(self): 
+
+class UserProduct():
+    def __init__(self):
         self.collection_name = "products"
 
-    def GetOne(self): 
+    def GetOne(self):
         pass
 
-    def GetAll(self): 
+    def GetAll(self):
         pass
 
-    def GetProducts(self, filter, sort, ascending=True, page=0):         
-        Database.find_all(self.collection_name, {'field':{'$regex':r'^the-regex'}}, sort, ascending, page)
-        pass
+    def GetProducts(self, filter, sort, ascending=True, page=0):
+        print("Trying to get all products with {} as filter and {} as sort".
+              format(filter, sort))
+        db = Database()
+        products = db.find_all(self.collection_name,
+                               {'name': {
+                                   '$regex': r'[a-zA-Z]*'
+                               }}, sort, ascending, page)
+
+        return
 
 
-class AdminProduct(): 
-    def __init__(self): 
+class AdminProduct(UserProduct):
+    def __init__(self):
         self.todo = "Implement this class with actions only an admin can do"
 
-    def create_product(self, name, price, image, digital, comment=""): 
-        pass 
-
-    def delete_product(self, id): 
+    def create_product(self, name, price, image, digital, comment=""):
         pass
 
-    def update_product(self, id, name, price, image, digital, comment=""): 
+    def delete_product(self, id):
         pass
 
-    
+    def update_product(self, id, name, price, image, digital, comment=""):
+        pass
