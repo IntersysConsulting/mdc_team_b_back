0b59f0f2812a13b301ead3a0816e18d0ef724861
rober <banda1915@gmail.com>
2019-09-25 16:46:05 -0500

    database

--- src/flaskr/app/modules/resources/reset_token/__init__.py
+++ src/flaskr/app/modules/resources/reset_token/__init__.py
@@ -11,7 +11,7 @@ class RevokedTokenModel():
         })
 
     def is_jti_blacklisted(jti):
-        query = Database.find(self.collection_name, jti)
+        query = self.db.find(self.collection_name, jti)
         if query:
             return "successful"
         else:
