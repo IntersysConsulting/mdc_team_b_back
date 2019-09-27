342b55f0fb8cea108875f1a1a822d8383dd026e4
Ricardo Figueroa <rfigueroa@intersysconsulting.com>
2019-09-27 13:28:51 -0500

    Update on products and removed unnecessary files

--- src/flaskr/app/modules/api/product/admin/post.py
+++ src/flaskr/app/modules/api/product/admin/post.py
@@ -2,7 +2,9 @@ from flask import jsonify
 from flask_restplus.namespace import RequestParser
 from werkzeug.datastructures import FileStorage
 from ....resources.product import AdminProduct
-
+from ....resources.validation import is_admin, is_not_admin_response
+from ....resources.images import upload_image
+import tempfile
 #################
 # Parser        #
 #################
@@ -34,7 +36,7 @@ Parser.add_argument('description',
 #################
 
 
-def Post(args):
+def Post(args, identity):
     # They are put into a python dictionary, we can access them like this.
     name = args['name']
     price = args['price']
@@ -45,23 +47,43 @@ def Post(args):
     description = 'No description' if not args['description'] else args[
         'description']  #Do this for optional fields
 
-    picture.save(picture.filename)
-
-    ap = AdminProduct()
-    success = ap.create_product(name,
-                                price,
-                                picture.filename,
-                                digital,
-                                description=description).acknowledged
-
-    if (success):
-        response = jsonify({
-            "statusCode": 200,
-            "message": "Successfully created a product",
-        })
+    if not is_admin(identity):
+        response = jsonify(is_not_admin_response)
     else:
-        response = jsonify({
-            "statusCode": 400,
-            "message": "There was an error creating a product",
-        })
+
+        tmpDir = tempfile.TemporaryDirectory()
+        image_dir = "{}\\{}".format(tmpDir.name, picture.filename)
+        print("Saving the picture in {}".format(image_dir))
+        picture.save(image_dir)
+        image_result = upload_image(image_dir)
+        print("Image result was: {}".format(image_result))
+        tmpDir.cleanup()
+
+        ap = AdminProduct()
+        success = ap.create_product(name,
+                                    price,
+                                    image_result["link"],
+                                    digital,
+                                    description=description).acknowledged
+
+        if (success):
+            if image_result['status'] == 200:
+                response = jsonify({
+                    "statusCode": 200,
+                    "message": "Successfully updated a product",
+                })
+            else:
+                response = jsonify({
+                    "statusCode":
+                    206,
+                    "message":
+                    "Successfully updated a product, but the image could not be updated.",
+                })
+        else:
+            response = jsonify({
+                "statusCode":
+                400,
+                "message":
+                "There was an error creating a product",
+            })
     return response
