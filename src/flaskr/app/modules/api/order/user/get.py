7070e1b5263a6734845fec87af0f8d90d0affe2d
Ricardo Figueroa <rfigueroa@intersysconsulting.com>
2019-10-03 13:37:22 -0500

    Implements GET over orders

--- src/flaskr/app/modules/api/order/user/get.py
+++ src/flaskr/app/modules/api/order/user/get.py
@@ -1,17 +1,17 @@
 from flask import jsonify
 from flask_restplus.namespace import RequestParser
-
+from ....resources.order import UserOrder
 #################
 # Parser        #
 #################
 Parser = RequestParser()
 Parser.add_argument('sort',
                     type=int,
-                    help='ID of the sorting method to be used',
+                    help='ID of the sorting method to be used (NYI)',
                     required=False)
 Parser.add_argument(
     'filter',
-    help='A comma separated string of all the filters that apply',
+    help='A comma separated string of all the filters that apply (NYI)',
     required=False)
 Parser.add_argument('page',
                     type=int,
@@ -27,14 +27,13 @@ def Get(args, identity):
     sort = 0 if not args['sort'] else args['sort']
     page = 0 if not args['page'] else args['page']
 
+    uo = UserOrder()
+    result = uo.get_user_orders(identity, filter, sort, page=page)
+    print("Result = {}".format(result))
+
     response = jsonify({
         "statusCode": 200,
         'message': 'Success',
-        "data": {
-            "Auth": identity,
-            "sort": sort,
-            "filter": filter,
-            "page": page
-        }
+        "data": result
     })
     return response
