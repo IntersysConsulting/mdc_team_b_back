f2b7e63ee45adb2c8d6719c45177739206c4190f
Ricardo Figueroa <rfigueroa@intersysconsulting.com>
2019-10-03 12:13:23 -0500

    Implemented orders PUT

--- src/flaskr/app/modules/api/order/user/put.py
+++ src/flaskr/app/modules/api/order/user/put.py
@@ -1,18 +1,22 @@
 from flask import jsonify
 from flask_restplus.namespace import RequestParser
-
+from ....resources.order import UserOrder
 #################
 # Parser        #
 #################
 Parser = RequestParser()
-Parser.add_argument('user_billing',
-                    help='User biling address JSON object',
-                    required=True,
-                    location='json')
-Parser.add_argument('user_shipping',
-                    help='User shipping address JSON object',
-                    required=True,
-                    location='json')
+Parser.add_argument(
+    'user_billing',
+    help="Index in the array of the user's billing addresses to be used.",
+    required=True,
+    type=int,
+    location='form')
+Parser.add_argument(
+    'user_shipping',
+    help="Index in the array of the user's billing addresses to be used.",
+    required=True,
+    type=int,
+    location='form')
 Parser.add_argument(
     'payment',
     help='User payment provider information. Depends on the provider',
@@ -25,19 +29,32 @@ Parser.add_argument(
 
 def Put(args, identity):
 
-    user_billing = args['user_billing']  #whole billing object
-    user_shipping = args['user_shipping']  #whole shipping object
+    user_billing_id = args['user_billing']  #whole billing object
+    user_shipping_id = args['user_shipping']  #whole shipping object
     payment = args['payment']  # Payment provider info
-    timestamp = datetime.now()  # Time at which the order was finished on
-    response = jsonify({
-        "statusCode": 200,
-        'message': 'Order succesfully added',
-        "data": {
-            "Auth": token,
-            "items": items,
-            "billing_id": user_billing,
-            "shipping_id": user_shipping,
-            "timestamp": timestamp.strftime("%d-%b-%Y (%H:%M:%S.%f)")
-        }
-    })
+
+    uo = UserOrder()
+    result = uo.finish_order(identity, user_shipping_id, user_billing_id,
+                             payment)
+
+    if result == -1:
+        response = jsonify({
+            "statusCode": 400,
+            'message': 'Customer does not have a POSTed order'
+        })
+    elif result == 0:
+        response = jsonify({
+            "statusCode": 400,
+            'message': 'Could not update order'
+        })
+    elif result == 1:
+        response = jsonify({
+            "statusCode": 200,
+            'message': 'Order succesfully updated'
+        })
+    else:
+        response = jsonify({
+            "statusCode": 400,
+            'message': 'Unexpected result={}'.format(result)
+        })
     return response
