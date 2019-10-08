5ebb5d64aa55c18c006658ac920d06086098e696
Ricardo <rfigueroa@intersysconsulting.com>
2019-10-08 15:39:09 -0500

    Added searching by name on product get

--- src/flaskr/app/modules/resources/product/__init__.py
+++ src/flaskr/app/modules/resources/product/__init__.py
@@ -3,6 +3,7 @@ from ...db import Database
 from .schema import ProductSchema
 from datetime import datetime
 from bson.objectid import ObjectId
+import re
 # In this file we implement methods that serve as a middle point between the exposed endpoint and the
 # actual middleware.
 # Validate operation logic here, not permission to access it.
@@ -14,19 +15,35 @@ class UserProduct():
         self.db = Database()
 
     def get_one(self, id):
-        return self.dump(self.db.find(self.collection_name, {"_id": ObjectId(id)}))
+        return self.dump(
+            self.db.find(self.collection_name, {"_id": ObjectId(id)}))
 
-    # def GetAll(self):
-    #     pass
+    def make_search(self, search):
+        pattern = '.*' + re.escape(search) + '.*'
+        return re.compile(pattern, re.IGNORECASE)
 
-    def get_products(self, filter, sort, ascending=True, page=0, page_size=None):
+    def get_products(self,
+                     search,
+                     digital,
+                     sort,
+                     ascending=True,
+                     page=0,
+                     page_size=None):
         output = []
-        selector = {'name': {'$regex': r''}}
+        if search != None:
+            selector = {'name': self.make_search(search)}
+        else:
+            selector = {'name': {'$regex': r''}}
 
-        total = self.db.get_count(self.collection_name, selector )
+        print("Trying to query with {}".format(selector))
+        total = self.db.get_count(self.collection_name, selector)
         print("The total I got was {}".format(total))
         products = self.db.find_all(self.collection_name,
-                                    selector, sort, ascending, page, page_size=page_size)
+                                    selector,
+                                    sort,
+                                    ascending,
+                                    page,
+                                    page_size=page_size)
         for product in products:
             output.append(self.dump(product))
 
